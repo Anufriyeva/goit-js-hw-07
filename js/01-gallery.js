db08f1accba30b823bdf3dac6bdcdc905edd188a
Evgeniya Anufriyeva <2281474@gmail.com>
2023-07-11 14:07:05 +0300

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,48 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryEl = document.querySelector(".gallery");
+const instance = basicLightbox.create("", {
+  onShow: () => {
+    document.addEventListener("keydown", keyboardEsc);
+  },
+  onClose: () => {
+    document.removeEventListener("keydown", keyboardEsc);
+  },
+});
+
+document.addEventListener("keydown", keyboardEsc);
+galleryEl.addEventListener("click", onClickElementGallery);
+
+function createElementGallery(items) {
+  return items.map(({ original, preview, description }) => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+      </a>
+    </li>
+  `).join("");
+}
+
+const galleryMarkup = createElementGallery(galleryItems);
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function onClickElementGallery(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  openOriginalSize(event.target.dataset.source);
+}
+
+function openOriginalSize(source) {
+  instance.element().innerHTML = `<img src="${source}" style="width: 60%; " />`;
+  instance.show();
+}
+
+function keyboardEsc(event) {
+  if (event.key === "Escape") {
+    instance.close();
+  }
+}
+
+console.log(galleryItems);
